e67f4d9678a28310a4386583d63af7071f3f3470
Tkino117 <kinoshi117@gmail.com>
2025-06-13 02:06:58 +0900

    renaming

---
+++ src/controller/aAuthController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import { UserService } from '../service/UserService';
+import { InvalidPasswordError, UserNotFoundError } from '../service/errors';
+import { SessionManager } from '../repository/SessionManager';
+import { EmptyResponseData, ErrorResponse, ServerErrorResponse, SuccessResponse, UnauthorizedResponse, UserResponseData } from '../response';
+import { handleError } from './util';
+
+export class AuthController {
+    constructor(private userService: UserService, private sessionManager: SessionManager) {
+    }
+
+    // sessionManagerへの依存をなくしたい!note!
+    async login(req: Request, res: Response) {
+        const userId: string = req.body.userId;
+        const password: string = req.body.password;
+        try {
+            await this.userService.login(userId, password);
+            const session = this.sessionManager.createSession(userId, new Date());
+            req.session.sessionId = session.sessionId;
+            const user = await this.userService.getUser(userId);
+            const data = new UserResponseData(user);
+            new SuccessResponse(data, 'Logged in').send(res);
+        }
+        catch(error: any) {
+            handleError(error, res);
+        }
+    }
+
+    async logout(req: Request, res: Response) {
+        try {
+        if(req.session.sessionId) {
+            await this.userService.logout(req.session.sessionId);
+            req.session.sessionId = '';
+            }
+            new SuccessResponse(new EmptyResponseData(), 'Logged out').send(res);
+        }
+        catch(error: any) {
+            handleError(error, res);
+        }
+    }
+}
